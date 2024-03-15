fa1cdc270f6ceacd622092a7ea19fdc579490171
24039285 <163328399+24039285@users.noreply.github.com>
2024-03-15 11:19:49 +0200

    Add files via upload

---
+++ subtract.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class subtract {
+
+    private static Object next;
+    public static void main(String[] args){
+        Scanner diff = new Scanner(System.in); 
+        
+        
+        System.out.println("enter num1");
+        int a = diff.nextInt();
+        
+        System.out.println("enter num2");
+        int b = diff.nextInt();
+        
+        int difference = a - b;
+        
+        System.out.println("The difference is "+ a +" and "+ b +" +difference");
+        
+       
+    
+    }
+
+    }
+    
+
